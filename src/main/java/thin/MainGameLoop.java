12d4000b626bdfa17453bb884a6007852bfa8bf5
I Am Groot <groot@localhost>
2024-12-12 23:26:08 -0500

    Added a square model.  What could be simpler

--- src/main/java/thin/MainGameLoop.java
+++ src/main/java/thin/MainGameLoop.java
@@ -11,6 +11,7 @@ import thin.resources.Loader;
 import thin.resources.Renderer;
 import thin.resources.items.Camera;
 import thin.resources.items.ModelItem;
+import thin.resources.model.HCLoader;
 import thin.resources.model.NewModel;
 import thin.resources.model.OBJLoader;
 import thin.resources.model.RawModel;
@@ -51,87 +52,12 @@ public class MainGameLoop {
         Vector3f mrot = new Vector3f(0.0f, 0.0f, 0.0f);
         Vector3f msca = new Vector3f(1.0f, 1.0f, 1.0f);
 
-        float [] verts = {
-            -0.5f, 0.5f,-0.5f,	
-            -0.5f,-0.5f,-0.5f,	
-             0.5f,-0.5f,-0.5f,	
-             0.5f, 0.5f,-0.5f,		
-            
-            -0.5f, 0.5f, 0.5f,	
-            -0.5f,-0.5f, 0.5f,	
-             0.5f,-0.5f, 0.5f,	
-             0.5f, 0.5f, 0.5f,
-            
-             0.5f, 0.5f,-0.5f,	
-             0.5f,-0.5f,-0.5f,	
-             0.5f,-0.5f, 0.5f,	
-             0.5f, 0.5f, 0.5f,
-            
-            -0.5f, 0.5f,-0.5f,	
-            -0.5f,-0.5f,-0.5f,	
-            -0.5f,-0.5f, 0.5f,	
-            -0.5f, 0.5f, 0.5f,
-            
-            -0.5f, 0.5f, 0.5f,
-            -0.5f, 0.5f,-0.5f,
-             0.5f, 0.5f,-0.5f,
-             0.5f, 0.5f, 0.5f,
-            
-            -0.5f,-0.5f, 0.5f,
-            -0.5f,-0.5f,-0.5f,
-             0.5f,-0.5f,-0.5f,
-             0.5f,-0.5f, 0.5f
-        };
-        float [] texuv = {
-            0,0,
-            0,1,
-            1,1,
-            1,0,
-            0,0,
-            0,1,
-            1,1,
-            1,0,
-            0,0,
-            0,1,
-            1,1,
-            1,0,
-            0,0,
-            0,1,
-            1,1,
-            1,0,
-            0,0,
-            0,1,
-            1,1,
-            1,0,
-            0,0,
-            0,1,
-            1,1,
-            1,0
-        };
-        int [] indxs = {
-            0,1,3,	
-            3,1,2,	
+        // RawModel m2 = HCLoader.loadHCModel(loader);
+        // TexturedModel m2t = new TexturedModel(m2, tw);
+        // ModelItem ment2 = new ModelItem(m2t, mloc, mrot, msca);
 
-            4,5,7,
-            7,5,6,
-
-            8,9,11,
-            11,9,10,
-
-            12,13,15,
-            15,13,14,	
-
-            16,17,19,
-            19,17,18,
-
-            20,21,23,
-            23,21,22
-        };
-        RawModel m2 = loader.loadToVAO(verts, texuv, indxs);
-        TexturedModel m2t = new TexturedModel(m2, tw);
-        ModelItem ment2 = new ModelItem(m2t, mloc, mrot, msca);
-
-        RawModel m3 = OBJLoader.loadOBJModel("src/main/res/mdls/Cube.obj", loader);
+        // RawModel m3 = OBJLoader.loadOBJModel("src/main/res/mdls/Cube.obj", loader);
+        RawModel m3 = OBJLoader.loadOBJModel("src/main/res/mdls/Square.obj", loader);
         TexturedModel m3t = new TexturedModel(m3, tw);
         ModelItem ment3 = new ModelItem(m3t, mloc, mrot, msca);
 
@@ -145,13 +71,14 @@ public class MainGameLoop {
             camera.move();
             shader.loadViewMatrix(camera);
 
-            ment2.move(new Vector3f(0.0f, 0.0f, -0.0001f));
-            ment2.turn(new Vector3f(0.0f, 0.01f, 0.01f));
-            renderer.render(ment2, shader);
+            // ment2.move(new Vector3f(0.0f, 0.0f, -0.0001f));
+            // ment2.turn(new Vector3f(0.0f, 0.01f, 0.01f));
+            // renderer.render(ment2, shader);
 
             ment3.move(new Vector3f(0.0f, 0.0f, -0.0001f));
-            ment3.turn(new Vector3f(0.0f, 0.01f, 0.01f));
+            ment3.turn(new Vector3f(0.0f, 0.001f, 0.001f));
             renderer.render(ment3, shader);
+
 
             shader.stop();
 
